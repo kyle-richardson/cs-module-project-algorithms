e2f3ee574419067cd02b6062e258b91c960a3874
Kyle Richardson <kyle.richardson1@gmail.com>
2020-08-13 22:29:43 -0500

    working on stretch

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -1,27 +1,42 @@
 #!/usr/bin/python
 
 import sys
+from collections import defaultdict
 
 
-def making_change(amount, denominations, cache={}):
-  # base case
+def making_change(amount, denominations, cache={}, previous=None):
+    # denominations = denominations.sort(reverse=True)
+    if previous is None:
+        previous = denominations[-1]
+    # print(f'start of method: amount: {amount}, previous: {previous}')
+    # base case
     if amount == 0:
+        # print(f'VALID')
         return 1
     elif amount < 0:
+        # print(f'not valid')
         return 0
-    elif amount in cache:
-        return cache[amount]
+
+    # check cache
+    # elif amount in cache and amount <= previous:
+    #     print(f'found in cache: {cache[amount]}')
+    #     return cache[amount]
+
+    # compute value and add to cache
     else:
         value = 0
-        for x in denominations:
-            value += making_change(amount - x, denominations)
+        removing_highs = [y for y in denominations if y <= previous]
+        # print(f"removing highs: {removing_highs}")
+        for x in removing_highs:
+            value += making_change(amount - x, denominations, previous=x)
+        # print(f'{amount}: {value}')
         cache[amount] = value
         return value
 
 
 if __name__ == "__main__":
-  # Test our your implementation from the command line
-  # with `python making_change.py [amount]` with different amounts
+    # Test our your implementation from the command line
+    # with `python making_change.py [amount]` with different amounts
     if len(sys.argv) > 1:
         denominations = [1, 5, 10, 25, 50]
         amount = int(sys.argv[1])
